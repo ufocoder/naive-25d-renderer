40181cb05bfeaa34bc80c770a53b3bb4aa2579b2
Sergey Ufocoder <xufocoder@gmail.com>
2026-07-05 11:25:18 +0300

    chore: improve content for stage-4*

--- src/pages/stage/_islands/Stage4c.tsx
+++ src/pages/stage/_islands/Stage4c.tsx
@@ -1,12 +1,12 @@
 import Canvas from "@app/components/Canvas/CanvasBase";
 import Map2d from "@app/components/Map2d";
-import { useBspTree } from '@app/stages/Stage3b/hooks/useBspTree';
-import { createRender25d } from '@app/stages/Stage4a//render25d';
-import type { Component } from 'solid-js';
-import { createSignal } from 'solid-js';
-import render2d from '@app/stages/Stage0b/render2d';
-import { useCameraControlsV4 } from '@app/stages/Stage4c/hooks/useCameraControls';
-import defaultSettings from '@app/stages/Stage4c/settings';
+import { useBspTree } from "@app/stages/Stage3b/hooks/useBspTree";
+import { createRender25d } from "@app/stages/Stage4a//render25d";
+import type { Component } from "solid-js";
+import { createSignal } from "solid-js";
+import render2d from "@app/stages/Stage0b/render2d";
+import { useCameraControlsV4 } from "@app/stages/Stage4c/hooks/useCameraControls";
+import defaultSettings from "@app/stages/Stage4c/settings";
 
 const Stage: Component = () => {
   const [settings, setSettings] = createSignal<Settings>(defaultSettings);
@@ -15,23 +15,23 @@ const Stage: Component = () => {
   useCameraControlsV4({ settings, setSettings, bspTree: bspTree() });
 
   return (
-          <div class="flex flex-col gap-4">
-            <div class="flex flex-col justify-center gap-6 md:grid md:grid-cols-2 md:gap-4 md:items-start justify-items">
-              <div class="flex flex-col gap-2">
-                <h2 class="flex justify-center text-2xl">2.5D Renderer</h2>
-                <div class="flex justify-center">
-                  <Canvas
+    <div class="flex flex-col gap-4">
+      <div class="flex flex-col justify-center gap-6 md:grid md:grid-cols-2 md:gap-4 md:items-start justify-items">
+        <div class="flex flex-col gap-2">
+          <h2 class="flex justify-center text-2xl">2.5D Renderer</h2>
+          <div class="flex justify-center">
+            <Canvas
               settings={settings}
               width={settings().camera.screen.width}
               height={settings().camera.screen.height}
               render={createRender25d({ bspTree: bspTree() })}
             />
-                </div>
-              </div>
-              <div class="flex flex-col gap-2">
-                <h2 class="flex justify-center text-2xl">2D Renderer</h2>
-                <div class="flex justify-center">
-                  <Map2d
+          </div>
+        </div>
+        <div class="flex flex-col gap-2">
+          <h2 class="flex justify-center text-2xl">2D Renderer</h2>
+          <div class="flex justify-center">
+            <Map2d
               withZoom
               initialZoom={0.5}
               initialOffsetX={20}
@@ -39,10 +39,10 @@ const Stage: Component = () => {
               settings={settings}
               render={render2d}
             />
-                </div>
-              </div>
-            </div>
           </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
