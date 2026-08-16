f251cf6dfac7220e1d3a9f9d594ea6a8ad26bfea
Sergey Ufocoder <xufocoder@gmail.com>
2026-08-16 15:25:20 +0300

    chore: refactor repo url usage

--- src/config/repo.ts
+++ src/config/repo.ts
@@ -1,1 +1,4 @@
-export const PROJECT_REPO_URL = 'https://github.com/ufocoder/25d-renderer/blob/main/src'
+export const PROJECT_REPO_URL = 'https://github.com/ufocoder/25d-renderer/blob/main/src';
+export const PROJECT_REPO_ROOT_URL = PROJECT_REPO_URL.replace('/blob/main/src', '');
+export const PROJECT_LICENSE_URL = `${PROJECT_REPO_ROOT_URL}/blob/main/LICENSE.md`;
+export const PROJECT_ISSUES_URL = `${PROJECT_REPO_ROOT_URL}/issues/new`;
